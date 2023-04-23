6aa019517c082dd7c2f2d6613be4d3c39e7bebf7
twk <1024565378@qq.com>
2023-04-23 15:35:02 +0800

    !

---
+++ 其他/clip.py
@@ -0,0 +1,53 @@
+# 2021年openai发布的模型，用于将文字描述和图片匹配。
+# 相当于1个图片分类模型与1个文本分类模型相结合，两个模型的标签一致，只是标签并非独热编码而是一段特征向量。
+# 因此一张图片经过图片分类模型得到的特征向量和这张图片的描述经过文本模型得到的特征向量相近，从而能够通过图片找文本，也可以通过文本找图片
+import PIL
+import clip
+import torch
+import transformers
+
+model_list = clip.available_models()
+print(model_list)
+# 模型
+model_name = 'ViT-L/14'  # 有多种型号，但中文文本模型只支持ViT-L/14
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, image_deal = clip.load(model_name, device=device)
+chinese_encode = transformers.BertForSequenceClassification.from_pretrained(
+    "IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese").eval().half().to(device)  # 中文语言特征提取模型，只支持ViT-L/14
+print(f'| 加载模型成功:{model_name} | 中文特征模型:IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese |')
+# 图片处理
+a = image_deal(PIL.Image.open("image/01.jpg"))
+b = image_deal(PIL.Image.open("image/02.jpg"))
+image_bacth = torch.stack([a, b], dim=0).to(device)
+print(f'| image_bacth.shape:{image_bacth.shape},{image_bacth.dtype} |')
+# 文本处理
+english_text = ['car', 'cat', 'a cat']
+chinese_text = ['车', '猫', '一只猫']
+# 英文
+english_text = clip.tokenize(english_text).to(device)
+print(f'| english_text.shape:{english_text.shape},{english_text.dtype} |')
+# 中文
+chinese_tokenizer = transformers.BertTokenizer.from_pretrained("IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese")
+chinese_text = chinese_tokenizer(chinese_text, max_length=77, padding='max_length', truncation=True,
+                                 return_tensors='pt')['input_ids'].type(torch.int32).to(device)
+print(f'| chinese_text.shape:{chinese_text.shape},{chinese_text.dtype} |')
+# 推理
+with torch.no_grad():
+    model = model.eval()
+    # 图片特征
+    image_feature = model.encode_image(image_bacth)
+    print(f'| image_feature.shape:{image_feature.shape},{image_feature.dtype} |')
+    # 英文文本特征
+    english_text_feature = model.encode_text(english_text)
+    print(f'| english_text_feature.shape:{english_text_feature.shape},{english_text_feature.dtype} |')
+    # 中文文本特征
+    chinese_text_feature = chinese_encode(chinese_text).logits
+    print(f'| chinese_text_feature.shape:{chinese_text_feature.shape},{chinese_text_feature.dtype} |')
+    # 图片和英文文本匹配
+    score = (image_feature @ english_text_feature.t())
+    score = torch.softmax(score, dim=1)
+    print("score(英文):", score)
+    # 图片和中文文本匹配
+    score = (image_feature @ chinese_text_feature.t())
+    score = torch.softmax(score, dim=1)
+    print("score(中文):", score)
